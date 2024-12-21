a6ff221e23f5e0da1baf9b4cf5a6210985c8d930
Nondumiso14 <Nondumisoshange3@gmail.com>
2024-12-21 11:20:29 -0800

    python_introduction

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,1 +1,8 @@
-chmod 755 simple_interest.py
+# A program to calculate the simple interest rate
+# Variabledefinition
+principal = 1000
+rate = 0.05
+time = 3
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}")
