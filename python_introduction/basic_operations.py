07e43b8d5f2983a9bb861103a38af50e98f4154f
Nondumiso14 <Nondumisoshange3@gmail.com>
2024-12-21 04:27:24 -0800

    python_introduction

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+#Calculating the sum
+number1 = 10
+number2 = 5
+sum = number1 + number2
+print (f"Addition of {number1} and {number2} is {sum}")
+# Calculating the difference
+Difference = number1 - number2
+print (f"Substration of {number1} and {number2} is {Difference}")
+#Calculating the product
+product = number1 * number2
+print (f"Multiplication of {number1} and {number2} is {product}")
