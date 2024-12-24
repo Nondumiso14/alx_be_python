55ad5929f75527a2dc3f3cdf247c53f8057b7b8b
Nondumiso14 <Nondumisoshange3@gmail.com>
2024-12-24 10:46:19 -0800

    control-flow

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,5 +1,5 @@
 weather = input("What's the weather like today? (sunny/rainy/cold):")
-if  weather  =="sunny":
+if  weather  =="sunny"or weather == "rainy" or weather == "cold" :
     print ("Wear a t-shirt and sunglasses.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
