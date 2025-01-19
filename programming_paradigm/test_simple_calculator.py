26208d2b88c1049a5b28e2192f04f50a43187683
Nondumiso14 <Nondumisoshange3@gmail.com>
2025-01-19 14:02:40 -0800

    alx_be_python

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,8 +1,4 @@
-import unittest
-from simple_calculator import SimpleCalculator
-
 class TestSimpleCalculator(unittest.TestCase):
-
     def setUp(self):
         """Set up the SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
@@ -11,27 +7,19 @@ class TestSimpleCalculator(unittest.TestCase):
         """Test the addition method."""
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
-        self.assertEqual(self.calc.add(-2, -2), -4)
-        self.assertEqual(self.calc.add(2, -1), 1)
-
+    
     def test_subtraction(self):
-        self.assertEqual(self.calc.subtraction(2, 3), -1)
-        self.assertEqual(self.calc.subtraction(0, 2) -2)
-        self.assertEqual(self.calc.subtraction(-3, -2), -1)
-        self.assertEqual(self.calc.subtraction(5, 0), 5)
+        """Test the subtraction method"""
+        self.assertEqual(self.calc.subtract(7, 4), 3)
+        self.assertEqual(self.calc.subtract(4, -3), 7)
 
-    def test_multiply(self):
-        self.assertEqual(self.calc.multiply(2, 3), 6)
-        self.assertEqual(self.calc.multiply(-1, 2), -2)
-        self.assertEqual(self.calc.multiply(0, 6), 0)
-        self.assertEqual(self.calc.multiply(-2, -4), 8)
+    def test_multiplication(self):
+        """Test the multiplication method"""
+        self.assertEqual(self.calc.multiply(4, 10), 40)
+        self.assertEqual(self.calc.multiply(5, -4), -20)
 
-    def test_divide(self):
-        self.assertEqual(self.calc.divide(4, 2), 2)
-        self.assertEqual(self.calc.divide(-2, 2), -1)
-        self.assertEqual(self.calc.divide(-5, -5), 1)
-        self.assertRaises(ZeroDivisionError, self.calc.divide, 10, 0)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_division(self):
+        """Test the division method and handle exception raises"""
+        self.assertEqual(self.calc.divide(15, 3), 5)
+        self.assertEqual(self.calc.divide(-12, 8), -1.5)
+        self.assertIsNone(self.calc.divide(9, 0))
