76d22497980444d9ae44c4b95f89f0c0bc3bd926
Nondumiso14 <Nondumisoshange3@gmail.com>
2025-01-19 14:22:09 -0800

    alx_be_python

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,3 +1,7 @@
+
+import unittest
+from simple_calculator import SimpleCalculator
+
 class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         """Set up the SimpleCalculator instance before each test."""
