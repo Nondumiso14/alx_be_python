4bb19fbed5a9a55199cc9c16e99c3ca0a4dfd0e1
Nondumiso14 <Nondumisoshange3@gmail.com>
2025-01-25 19:27:01 -0800

    alx_be_python

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,35 @@
+class Calculator:
+    calculation_type = "Arithmetic Operations"
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def add(a: float, b: float) -> float:
+        """
+        Returns the sum of two numbers.
+        
+        Args:
+        a (float): The first number.
+        b (float): The second number.
+        
+        Returns:
+        float: The sum of a and b.
+        """
+        return a + b
+
+    @classmethod
+    def multiply(cls, a: float, b: float) -> float:
+        """
+        Returns the product of two numbers.
+
+        Args:
+        a (float): The first number.
+        b (float): The second number.
+
+        Returns:
+        float: The product of a and b.
+        """
+        print(f"Performing {cls.calculation_type.lower()}:")
+        return a * b
+
